92311ee2830f2aa065f16cfe8cbc498415ced05b
wilson18 <wilsongarcianova18@gmail.com>
2022-11-11 10:40:18 -0400

    asarando con el spinner

---
+++ gesin-app/wwwroot/js/Recuperar_style_usuarios.js
@@ -0,0 +1,28 @@
+﻿
+
+
+
+//esta funcion nos recupera el background color en la db
+const recuperarStyle = () => {
+
+    const url = "/Reportes/RecuperarStyle"
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+
+
+            let area_averias = document.getElementById("area_averias");
+            area_averias.style.backgroundColor = `${data.background}`;
+
+            let head_table = document.getElementById("head_table");
+            head_table.style.backgroundColor = `${data.background}`;
+
+
+            let inputColor = document.getElementById("inputcolor");
+            inputColor.value = `${data.background}`;
+        })
+
+
+}
+
+recuperarStyle();
